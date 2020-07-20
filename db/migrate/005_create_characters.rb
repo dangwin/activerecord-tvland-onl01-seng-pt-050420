4528c9c940ce66bbc57b7e92c82704b3f2a08da5
Dang Nguyen <danghwin@gmail.com>
2020-07-20 03:28:06 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -3,7 +3,7 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
     def change
         create_table :characters do |t|
             t.string :name
-            t.string :actor
+            t.integer :actor_id
             t.integer :show_id
         end
     end
